e599f452f22e1f439a2908cf2fca68c84337cbce
Andrew <andrewrpelton@outlook.com>
2025-12-07 09:59:06 -0800

    Added a ghost texture

--- Ghost.py
+++ Ghost.py
@@ -3,14 +3,24 @@ from Enemy import Enemy
 from GameObject import GameObject
 from Healthbar import Healthbar
 import pygame
+import math
 
 class Ghost(GameObject):
     def __init__(self, x:float, y:float, target:Enemy) -> None:
+        super().__init__()
         self.x = x
         self.y = y
         self.target = target
         self.speed = 130
-        super().__init__()
+        self.image = self.load_image("Assets/ghost.png", (60,60))
+        
 
     def render(self, screen:pygame.Surface):
-        pygame.draw.circle(screen, "darkorchid2", (self.x, self.y), 20)
+        x_component = self.speed * math.cos(self.direction)
+
+        #if x_component > 0:
+        #    rotated_image = pygame.transform.rotate(self.image,-35)
+        #else:
+        #    rotated_image = pygame.transform.rotate(self.image, 35)
+
+        screen.blit(self.image, (self.x, self.y))
